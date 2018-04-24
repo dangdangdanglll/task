82d567949864755727c35621599651d9bd748a7e
dangdangdanglll <33782604+dangdangdanglll@users.noreply.github.com>
2018-04-24 17:14:19 +0800

    Add files via upload

--- 未命名1.cpp
+++ 未命名1.cpp
@@ -1,23 +1,5 @@
 #include<stdio.h>
-int main()
+main()
 {
-	int q,w;
-	printf("请输入成绩（输入0结束）：");
-	scanf("%d",&q);
-	while(w!=0)
-	{
-		w=q/10;
-		switch(w)
-		{
-			case 10 :printf("优");break;
-			case 9 :printf("优") ;break;
-			case 8 :printf("良");break;
-			case 7: printf("中");break;
-			case 6: printf("及格");break; 
-			default :printf("不及格");
-			
-		}
-		scanf("%d",&q) ;
-	}
-	
-}
+	printf("hello world"); 
+ } 
